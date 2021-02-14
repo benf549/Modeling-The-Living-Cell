2958c02b5932e540ff2e023590e26a1c1afd4e4e
Benf549 <benjaminpfry@gmail.com>
2021-02-13 23:56:33 -0500

    completed PS2 parts 1 and 2. working on part 3

--- PS2/main.m
+++ PS2/main.m
@@ -2,20 +2,20 @@
 %bfry2
 %Problem Set 2 - Modeling The Living Cell
 
-%% Random Number Generation and Seeding
+%% 1. Random Number Generation and Seeding
 rng('shuffle')                          
 
-%Adjust random normal distribution to mean of 5 and std.dev 2
+%a. Adjust random normal distribution to mean of 5 and std.dev 2
 r = 5 + 2.*randn(1000, 1);
 
-%Plot histogram of Counts
+%b. Plot histogram of Counts
 figure(1)
 histogram(r,35)
 title('Frequency Plot For 1000 Points Sampled From N(5,4) Distribution');
 ylabel('Number of Values Observed');
 xlabel('Value Observed');
 
-%Plot histogram of counts as normalized pdf. 
+%c. Plot histogram of counts as normalized pdf. 
 figure(2)
 histogram(r,35, 'Normalization', 'pdf')
 title('PDF of 1000 Points Sampled From N(5,4) Distribution');
@@ -34,15 +34,61 @@ disp(sum(N) * binsize)
 aa_key = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', ...
     'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'];
 
-%Generate a 300 by 1 matrix of random integers between [1, 20]
+%Generate a 300 by 1 matrix of random integers on interval [1, 20]
 num_seq = randi(20, 300, 1);
-%histogram(sequence)
-char_seq = aa_key(num_seq);
+figure(1)
+histogram(num_seq, 'Normalization', 'pdf')
+set(gca,'xtick',[1:20],'xticklabel',aa_key(1:20)');
+equal_freq_char_seq = aa_key(num_seq)
+title("Residue Frequency in Randomly Generated 300 Residue String Assuming All Have Equal Liklihood")
+xlabel("Amino Acid (One Letter Code)")
+ylabel("Residue Frequncy (count/string length)")
 
 %True Frequencies
 true_freqs = [8.25, 5.53, 4.06, 5.45, 1.37, 3.93, 6.75, 7.07, 2.27, 5.96, ...
     9.66, 5.84, 2.42, 3.86, 4.70, 6.56, 5.34, 1.08, 2.92, 6.87] ./ 100;
-sum(true_freqs)
+sum(true_freqs);
 
+%Calculate Cumulative Sum and plot CDF
 cumul = cumsum(true_freqs);
-cdfplot(true_freqs)
+figure(2)
+h = stairs(1:20, cumul);
+set(gca,'xtick',[1:20],'xticklabel',aa_key(1:20)');
+title("Cumulative Distribution Function For Amino Acid Probabilities")
+xlabel("Amino Acid (One Letter Code)")
+ylabel("Probability")
+
+%Generate 300 random numbers and use CDF to convert to residues
+rand300 = rand(300,1);
+result = zeros(1,length(rand300));
+for i = 1:length(rand300)
+    result(i) = getRes(rand300(i), true_freqs);
+end
+emperical_freq_char_seq = aa_key(result)
+
+figure(3)
+histogram(result, 'Normalization', 'pdf');
+[model_freqs, edges] = histcounts(result, 'Normalization', 'pdf');
+set(gca,'xtick',1:20,'xticklabel',aa_key(1:20)');
+title("Residue Frequency in Randomly Generated 300 Residue String Using Expasy Residue Frequencies")
+ylabel("Residue Frequncy (count/string length)")
+xlabel("Amino Acid (One Letter Code)")
+
+function resi = getRes(num, frequencies)
+%Takes in a randomly generated number between zero and 1 and a vector of
+%frequencies. Computes the cumulative sum and steps through each index to
+%identify the residue corresponds to the random number. 
+    resi = 1;
+    for i = cumsum(frequencies)
+        if num < i
+            break
+        elseif num > 0.9989 %Because probabilities dont add to 1, add remaining ~0.001 to V
+            resi = 20;
+            break
+        else
+            resi = resi + 1;
+        end
+    end
+end
+
+%%
