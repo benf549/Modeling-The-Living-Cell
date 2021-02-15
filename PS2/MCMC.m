14b80c126da2acd470fa9241b117649d132c7fc2
Benf549 <benjaminpfry@gmail.com>
2021-02-15 01:37:45 -0500

    Reworked energy  calculation and order of magnitude appears fixed. Also reworked MCMC algorithm to avoid unnecessary function calls which saves a significant amount of calculation time. Question 3 is pretty much finished unless the TAs point something out in OH

--- PS2/MCMC.m
+++ PS2/MCMC.m
@@ -1,4 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Benjamin Fry (bfry2)
+% 2/14/21, coded on MATLAB _R2020b_ 
+% 
+% Performs a Markov Chain Monte Carlo simulation on a matrix of R^3 vectors.
+% Applies the Monte Carlo algorithm and generates a vector of Lennard-Jones
+% potential energies representing the energies of the system for each step
+% in random walk.
+%
+% Output is a vector of energies corresponding to the energy calculated for
+% each step in the random walk. 
+% 
+% Input is the number of steps in random walk, the algorithm parameters
+% delta, beta, L, epsilon, and sigma as explained in Frenkel and Smit ch 3,
+% and the matrix of R^3 vectors on which the random walk will be performed.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [energy_array] = MCMC(num_cycles, delta, beta, L, epsilon, sigma, data_matrix)
+    
     temp_out = zeros(1, num_cycles);
     
     for i = 1:num_cycles
@@ -7,45 +28,39 @@ function [energy_array] = MCMC(num_cycles, delta, beta, L, epsilon, sigma, data_
         selection = randi([1,30]);
         old = [data_matrix(selection, 1) data_matrix(selection, 2) data_matrix(selection, 3)];
     
-        %calculate energy of old state
-        old_energy = 0;
-        for j = 1:30
-            if j ~= selection
-                old_energy = old_energy + calc_LJ_Potential(L, epsilon, sigma, old, [data_matrix(j,1) data_matrix(j,2) data_matrix(j,3)]);
-            end
+        %calculate energy of old state. Calculate it if first call,
+        %otherwise use memoized value to avoid repeat calculation
+        if i > 1 
+            old_energy = temp_out(i-1);
+        else
+            old_energy = calc_LJ_Potential(L, epsilon, sigma, data_matrix);
         end
-        disp(old_energy)
+        
         %Randomly independently adjust x, y, z coordinates of old
-        new = zeros(1,3);
-        new(1) = old(1) + delta*(rand()-0.5);
-        new(2) = old(2) + delta*(rand()-0.5);
-        new(3) = old(3) + delta*(rand()-0.5);
+        data_matrix(selection, 1) = old(1) + delta*(rand()-0.5);
+        data_matrix(selection, 2) = old(2) + delta*(rand()-0.5);
+        data_matrix(selection, 3) = old(3) + delta*(rand()-0.5);
     
         %Calculate energy of new state
-        new_energy = 0;
-        for j = 1:30
-            if j ~= selection
-                new_energy = new_energy + calc_LJ_Potential(L, epsilon, sigma, new, [data_matrix(j,1) data_matrix(j,2) data_matrix(j,3)]);
-            end
-        end
-
+        new_energy = calc_LJ_Potential(L, epsilon, sigma, data_matrix);
+        
         %Define acceptance criterion with Boltzmann factor.
         acc = min([1, exp(-beta * (new_energy - old_energy))]);
-    
+        
         %If a randomly generated number is less than the acceptance criterion, we accept the old to new transition
         if rand() < acc
-            %Update old in data matrix with new value
-            data_matrix(selection, 1) = new(1); 
-            data_matrix(selection, 2) = new(2); 
-            data_matrix(selection, 3) = new(3); 
-
             %Append the calculated energy of the new state to energy array for calculation of average
             temp_out(i) = new_energy;
+
         else
-            %If we do not accept the transition, we keep the old energy and append to energy array for average calculation
+            %If we do not accept the transition, we keep the old position and energy and append to energy array for average calculation
+            data_matrix(selection, 1) = old(1);
+            data_matrix(selection, 2) = old(2);
+            data_matrix(selection, 3) = old(3);
             temp_out(i) = old_energy;
         end
     end
+    %return array of calculated energies.
     energy_array = temp_out;
 end
 
--- PS2/calc_LJ_Potential.m
+++ PS2/calc_LJ_Potential.m
@@ -7,33 +7,39 @@
 % Calculates the Lennard Jones Potential for a box of particles using the
 % minimum image convention for periodic boundary conditions. 
 
-% Output is a double corresponding to the potential energy from the interaction between
-% the two vectors ri and rj. 
+% Output is a double corresponding to the potential energy of the unique pairwise
+% interactions of the particles
 % 
 % Input is L - the length of the box, epsilon -
-% the reduced energy unit, sigma - the reduced unit of length and the two
-% R^3 vectors ri and rj you want to find the potential between. 
+% the reduced energy unit, sigma - the reduced unit of length and the
+% matrix of values for which to sum over
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function v = calc_LJ_Potential(L, epsilon, sigma, ri, rj)
+function V = calc_LJ_Potential(L, epsilon, sigma, data_matrix)
+    v = 0;
+    for i = 1:length(data_matrix)
+        for j = i+1:length(data_matrix)
+            
+            %Calculate the difference between each vector component
+            Deltx = data_matrix(i,1) - data_matrix(j,1);
+            Delty = data_matrix(i,2) - data_matrix(j,2);
+            Deltz = data_matrix(i,3) - data_matrix(j,3);
 
-    %Calculate the difference between each vector component
-    Deltx = ri(1) - rj(1);
-    Delty = ri(2) - rj(2);
-    Deltz = ri(3) - rj(3);
-    
-    %Since the delta cannot exceed L, dividing the delta by L and
-    %casting to int gives -1/0/1 as possible values. Then multiply by L to
-    %get the component's nearest image distance on subtraction from delta.
-    Deltx = Deltx - L*double(int8(Deltx/L));
-    Delty = Delty - L*double(int8(Delty/L));
-    Deltz = Deltz - L*double(int8(Deltz/L));
-    
-    %Calculate magnitude of the distance between vectors
-    dist = sqrt(Deltx^2 + Delty^2 + Deltz^2);
+            %Since the delta cannot exceed L, dividing the delta by L and
+            %casting to int gives -1/0/1 as possible values. Then multiply by L to
+            %get the component's nearest image distance on subtraction from delta.
+            Deltx = Deltx - L*double(int8(Deltx/L));
+            Delty = Delty - L*double(int8(Delty/L));
+            Deltz = Deltz - L*double(int8(Deltz/L));
 
-    %Apply Lennard-Jones Potential formula and return calculated potential.
-    v = 4*epsilon*( (sigma/dist)^12 - (sigma/dist)^6 );
+            %Calculate magnitude of the distance between vectors
+            dist = sqrt(Deltx^2 + Delty^2 + Deltz^2);
+
+            %Apply Lennard-Jones Potential formula and return calculated potential.
+            v = v + 4*epsilon*( (sigma/dist)^12 - (sigma/dist)^6 );
+        end
+    end
+    V = v;
 end
